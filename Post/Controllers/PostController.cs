950cdc51a115db18059abe16dd42ee48ebb98c5c
ayykapu <kurkeu@gmail.com>
2023-11-05 17:36:00 +0100

    post

---
+++ Post/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Post.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Post/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Post.Models
+{
+    public class Post
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Display(Name = "Zawartość")]
+        [Required(ErrorMessage = "Musisz podać zawartość wpisu!")]
+        [StringLength(maximumLength: 150, ErrorMessage = "Post jest zbyt długi!")]
+        public string Name { get; set; }
+    }
+}
