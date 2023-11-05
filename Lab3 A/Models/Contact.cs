c55bef28491e29420a7afee1e52fb1b4e77a2d3c
ayykapu <kurkeu@gmail.com>
2023-11-05 16:18:44 +0100

    fix

---
+++ Lab3 A/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3_A.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Musisz podać imię!")]
+        [StringLength(maximumLength:50, ErrorMessage = "Imię jest zbyt długie!")]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        public DateTime? Birth { get; set; }
+    }
+}
--- Lab3_18.10.2023 A/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab3_18._10._2023.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
